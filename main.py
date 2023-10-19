a3a35ba4ae397c6e5c2bf0b3976c10d14ce73483
daaaz1 <daaa-z1@users.noreply.github.com>
2023-10-19 12:04:11 +0700

    -m

--- main.py
+++ main.py
@@ -1,23 +1,27 @@
 import sys
-from PyQt5.QtCore import Qt, QUrl
+from PyQt5.QtCore import Qt, QUrl, pyqtProperty, pyqtSignal, pyqtSlot
 from PyQt5.QtQml import QQmlApplicationEngine
 from PyQt5.QtWidgets import QApplication
 from u6 import U6
 
+class LabJackU6Reader:
+    def __init__(self):
+        self.u6 = U6()
+
+    def readAIN0(self):
+        value = self.u6.getAIN(0)
+        return value
+
 if __name__ == "__main__":
     app = QApplication(sys.argv)
 
-    # Inisialisasi LabJack U6
-    with U6() as u6:
-        # Membaca nilai AIN0
-        u6Value = u6.getAIN(0)
+    engine = QQmlApplicationEngine()
 
-        # Menginisialisasi QML Engine
-        engine = QQmlApplicationEngine()
-        engine.rootContext().setContextProperty("u6Value", u6Value)
+    # Membuat instance dari kelas LabJackU6Reader
+    reader = LabJackU6Reader()
+    engine.rootContext().setContextProperty("labJackReader", reader)
 
-        # Memuat berkas QML
-        engine.load(QUrl("main.qml"))
+    # Memuat berkas QML
+    engine.load(QUrl("main.qml"))
 
-        # Menampilkan GUI
-        sys.exit(app.exec_())
+    sys.exit(app.exec_())
