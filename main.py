0659aeb3a7557c084bdadce9356936b2dd1786ef
daaaz1 <daaa-z1@users.noreply.github.com>
2023-10-19 15:35:20 +0700

    m

--- main.py
+++ main.py
@@ -1,27 +1,29 @@
 import sys
-from PyQt5.QtCore import Qt, QUrl, pyqtProperty, pyqtSignal, pyqtSlot
+from PyQt5.QtCore import QObject, pyqtSlot, QTimer, QUrl
 from PyQt5.QtQml import QQmlApplicationEngine
 from PyQt5.QtWidgets import QApplication
-from u6 import U6
+from PyQt5.QtQuick import QQuickView
+import labjack
 
-class LabJackU6Reader:
+class LabJackReader(QObject):
     def __init__(self):
-        self.u6 = U6()
+        super().__init__()
+        self.labjack_device = labjack.LabJack()
+        self.timer = QTimer(self)
+        self.timer.timeout.connect(self.read_ain0)
+        self.timer.start(1000)  # Baca setiap 1 detik
 
-    def readAIN0(self):
-        value = self.u6.getAIN(0)
+    @pyqtSlot(result=float)
+    def read_ain0(self):
+        value = self.labjack_device.readAIN(0)
         return value
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     app = QApplication(sys.argv)
-
     engine = QQmlApplicationEngine()
 
-    # Membuat instance dari kelas LabJackU6Reader
-    reader = LabJackU6Reader()
-    engine.rootContext().setContextProperty("labJackReader", reader)
+    labjack_reader = LabJackReader()
+    engine.rootContext().setContextProperty("labjackReader", labjack_reader)
 
-    # Memuat berkas QML
     engine.load(QUrl("main.qml"))
-
     sys.exit(app.exec_())
--- main.qml
+++ main.qml
@@ -3,17 +3,24 @@ import QtQuick.Controls 2.0
 
 ApplicationWindow {
     visible: true
-    width: 400
-    height: 200
-    title: "LabJack U6 AIN0 Reader"
+    width: 640
+    height: 480
+    title: "Baca AIN0 LabJack"
 
     Rectangle {
-        anchors.fill: parent
-        color: "lightgray"
+        width: parent.width
+        height: parent.height
 
         Text {
+            text: "Nilai AIN0 dari LabJack:"
+            anchors.top: parent.top
+            anchors.horizontalCenter: parent.horizontalCenter
+        }
+
+        Text {
+            text: labjackReader.read_ain0().toFixed(2)
+            font.pixelSize: 24
             anchors.centerIn: parent
-            text: "AIN0 Value: " + u6Value
         }
     }
 }
