462db01dba44595970aa1a7d8543d72c203908c4
daaaz1 <daaa-z1@users.noreply.github.com>
2023-11-15 13:47:31 +0700

    jkh

--- koneksi.py
+++ koneksi.py
@@ -21,27 +21,6 @@ def buat_tabel_konfigurasi(koneksi):
         except sqlite3.Error as e:
             print(f"Kesalahan dalam membuat tabel Configurations: {e}")
 
-# Fungsi untuk membuat tabel pengukuran jika belum ada
-def buat_tabel_pengukuran(koneksi):
-    if koneksi:
-        try:
-            cursor = koneksi.cursor()
-            cursor.execute('''CREATE TABLE IF NOT EXISTS Measurements
-                              (ID INTEGER PRIMARY KEY AUTOINCREMENT,
-                               Config_ID INTEGER NOT NULL,
-                               Pressure_In INTEGER,
-                               Pressure_A INTEGER,
-                               Pressure_B INTEGER,
-                               Flow INTEGER,
-                               Temp INTEGER,
-                               Curr_V INTEGER,
-                               Aktual INTEGER,
-                               Curr_MA INTEGER,
-                               FOREIGN KEY (Config_ID) REFERENCES Configurations (ID))''')
-            koneksi.commit()
-        except sqlite3.Error as e:
-            print(f"Kesalahan dalam membuat tabel Measurements: {e}")
-
 # Fungsi untuk membuat tabel batasan jika belum ada
 def buat_tabel_batasan(koneksi):
     if koneksi:
@@ -66,6 +45,10 @@ def buat_tabel_batasan(koneksi):
                                Aktual_Max INTEGER,
                                Curr_MA_Min INTEGER,
                                Curr_MA_Max INTEGER,
+                               Press_Com_Min INTEGER,
+                               Press_Com_Max INTEGER,
+                               Press_Aktual_Min INTEGER,
+                               Press_Aktual_Max INTEGER,
                                FOREIGN KEY (Config_ID) REFERENCES Configurations (ID))''')
             koneksi.commit()
         except sqlite3.Error as e:
@@ -95,6 +78,10 @@ def buat_tabel_scaling(koneksi):
                                Aktual_Scale_Max INTEGER,
                                Curr_MA_Scale_Min INTEGER,
                                Curr_MA_Scale_Max INTEGER,
+                               Press_Com_Scale_Min INTEGER,
+                               Press_Com_Scale_Max INTEGER,
+                               Press_Aktual_Scale_Min INTEGER,
+                               Press_Aktual_Scale_Max INTEGER,
                                FOREIGN KEY (Config_ID) REFERENCES Configurations (ID))''')
             koneksi.commit()
         except sqlite3.Error as e:
@@ -114,6 +101,59 @@ def buat_tabel_switch(koneksi):
                               Btn4 INTEGER,
                               Btn5 INTEGER,
                               Btn6 INTEGER,
+                              Btn7 INTEGER,
+                              Btn8 INTEGER,
+                              Btn9 INTEGER,
+                              Btn10 INTEGER,
+                              Btn11 INTEGER,
+                              Btn12 INTEGER,
+                              Btn13 INTEGER,
+                              Btn14 INTEGER,
+                              Btn15 INTEGER,
+                              Btn16 INTEGER,
+                              Btn17 INTEGER,
+                              Btn18 INTEGER,
+                              Btn19 INTEGER,
+                              Btn20 INTEGER,
+                              Btn21 INTEGER,
+                              Btn22 INTEGER,
+                              Btn23 INTEGER,
+                              FOREIGN KEY (Config_ID) REFERENCES Configurations (ID))''')
+            koneksi.commit()
+        except sqlite3.Error as e:
+            print(f"Kesalahan dalam membuat tabel Switch: {e}")
+
+# Fungsi untuk membuat tabel fungsi tombol
+def buat_tabel_switch(koneksi):
+    if koneksi:
+        try:
+            cursor = koneksi.cursor()
+            cursor.execute('''CREATE TABLE IF NOT EXISTS State
+                              (ID INTEGER PRIMARY KEY AUTOINCREMENT,
+                              Config_ID INTEGER NOT NULL,
+                              Btn1_State INTEGER,
+                              Btn2_State INTEGER,
+                              Btn3_State INTEGER,
+                              Btn4_State INTEGER,
+                              Btn5_State INTEGER,
+                              Btn6_State INTEGER,
+                              Btn7_State INTEGER,
+                              Btn8_State INTEGER,
+                              Btn9_State INTEGER,
+                              Btn10_State INTEGER,
+                              Btn11_State INTEGER,
+                              Btn12_State INTEGER,
+                              Btn13_State INTEGER,
+                              Btn14_State INTEGER,
+                              Btn15_State INTEGER,
+                              Btn16_State INTEGER,
+                              Btn17_State INTEGER,
+                              Btn18_State INTEGER,
+                              Btn19_State INTEGER,
+                              Btn20_State INTEGER,
+                              Btn21_State INTEGER,
+                              Btn22_State INTEGER,
+                              Btn23_State INTEGER,
                               FOREIGN KEY (Config_ID) REFERENCES Configurations (ID))''')
             koneksi.commit()
         except sqlite3.Error as e:
@@ -131,24 +171,24 @@ def tambah_konfigurasi(koneksi, nama):
             print(f"Kesalahan dalam menambahkan konfigurasi: {e}")
     return None
 
-# Fungsi untuk menambahkan data pengukuran
-def tambah_pengukuran(koneksi, config_id, data):
-    if koneksi:
-        try:
-            cursor = koneksi.cursor()
-            cursor.execute("INSERT INTO Measurements (Config_ID, Pressure_In, Pressure_A, Pressure_B, Flow, Temp, Curr_V, Aktual, Curr_MA) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)",
-                           (config_id, data["Pressure_In"], data["Pressure_A"], data["Pressure_B"], data["Flow"], data["Temp"], data["Curr_V"], data["Aktual"], data["Curr_MA"]))
-            koneksi.commit()
-        except sqlite3.Error as e:
-            print(f"Kesalahan dalam menambahkan pengukuran: {e}")
-
 # Fungsi untuk menambahkan data switch
 def tambah_switch(koneksi, config_id, btn):
     if koneksi:
         try:
             cursor = koneksi.cursor()
-            cursor.execute("INSERT INTO Switch (Config_ID, Btn1, Btn2, Btn3, Btn4, Btn5, Btn6) VALUES (?, ?, ?, ?, ?, ?, ?)",
-                           (config_id, btn["Btn1"], btn["Btn2"], btn["Btn3"], btn["Btn4"], btn["Btn5"], btn["Btn6"]))
+            cursor.execute("INSERT INTO Switch (Config_ID, Btn1, Btn2, Btn3, Btn4, Btn5, Btn6, Btn7, Btn8, Btn9, Btn10, Btn11, Btn12, Btn13, Btn14, Btn15, Btn16, Btn17, Btn18, Btn19, Btn20, Btn21, Btn22, Btn23) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)",
+                           (config_id, btn["Btn1"], btn["Btn2"], btn["Btn3"], btn["Btn4"], btn["Btn5"], btn["Btn6"], btn["Btn7"], btn["Btn8"], btn["Btn9"], btn["Btn10"], btn["Btn11"], btn["Btn12"], btn["Btn13"], btn["Btn14"], btn["Btn15"], btn["Btn16"], btn["Btn17"], btn["Btn18"], btn["Btn19"], btn["Btn20"], btn["Btn21"], btn["Btn22"], btn["Btn23"]))
+            koneksi.commit()
+        except sqlite3.Error as e:
+            print(f"Kesalahan dalam menambahkan switch: {e}")
+
+# Fungsi untuk menambahkan data state
+def tambah_state(koneksi, config_id, state):
+    if koneksi:
+        try:
+            cursor = koneksi.cursor()
+            cursor.execute("INSERT INTO Switch (Config_ID, Btn1_state, Btn2_state, Btn3_state, Btn4_state, Btn5_state, Btn6_state, Btn7_state, Btn8_state, Btn9_state, Btn10_state, Btn11_state, Btn12_state, Btn13_state, Btn14_state, Btn15_state, Btn16_state, Btn17_state, Btn18_state, Btn19_state, Btn20_state, Btn21_state, Btn22_state, Btn23_state) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)",
+                           (config_id, state["Btn1"], state["Btn2"], state["Btn3"], state["Btn4"], state["Btn5"], state["Btn6"], state["Btn7"], state["Btn8"], state["Btn9"], state["Btn10"], state["Btn11"], state["Btn12"], state["Btn13"], state["Btn14"], state["Btn15"], state["Btn16"], state["Btn17"], state["Btn18"], state["Btn19"], state["Btn20"], state["Btn21"], state["Btn22"], state["Btn23"]))
             koneksi.commit()
         except sqlite3.Error as e:
             print(f"Kesalahan dalam menambahkan switch: {e}")
@@ -158,8 +198,8 @@ def tambah_batasan(koneksi, config_id, batasan):
     if koneksi:
         try:
             cursor = koneksi.cursor()
-            cursor.execute("INSERT INTO Limits (Config_ID, Pressure_In_Min, Pressure_In_Max, Pressure_A_Min, Pressure_A_Max, Pressure_B_Min, Pressure_B_Max, Flow_Min, Flow_Max, Temp_Min, Temp_Max, Curr_V_Min, Curr_V_Max, Aktual_Min, Aktual_Max, Curr_MA_Min, Curr_MA_Max) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)",
-                           (config_id, batasan["Pressure_In_Min"], batasan["Pressure_In_Max"], batasan["Pressure_A_Min"], batasan["Pressure_A_Max"], batasan["Pressure_B_Min"], batasan["Pressure_B_Max"], batasan["Flow_Min"], batasan["Flow_Max"], batasan["Temp_Min"], batasan["Temp_Max"], batasan["Curr_V_Min"], batasan["Curr_V_Max"], batasan["Aktual_Min"], batasan["Aktual_Max"], batasan["Curr_MA_Min"], batasan["Curr_MA_Max"]))
+            cursor.execute("INSERT INTO Limits (Config_ID, Pressure_In_Min, Pressure_In_Max, Pressure_A_Min, Pressure_A_Max, Pressure_B_Min, Pressure_B_Max, Flow_Min, Flow_Max, Temp_Min, Temp_Max, Curr_V_Min, Curr_V_Max, Aktual_Min, Aktual_Max, Curr_MA_Min, Curr_MA_Max, Press_Com_Min, Press_Com_Max, Press_Aktual_min, Press_Aktual_max) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)",
+                           (config_id, batasan["Pressure_In_Min"], batasan["Pressure_In_Max"], batasan["Pressure_A_Min"], batasan["Pressure_A_Max"], batasan["Pressure_B_Min"], batasan["Pressure_B_Max"], batasan["Flow_Min"], batasan["Flow_Max"], batasan["Temp_Min"], batasan["Temp_Max"], batasan["Curr_V_Min"], batasan["Curr_V_Max"], batasan["Aktual_Min"], batasan["Aktual_Max"], batasan["Curr_MA_Min"], batasan["Curr_MA_Max"], batasan["Press_Com_Min"], batasan["Press_Com_Max"], batasan["Press_Aktual_Min"], batasan["Press_Aktual_Max"]))
             koneksi.commit()
         except sqlite3.Error as e:
             print(f"Kesalahan dalam menambahkan batasan: {e}")
@@ -169,8 +209,8 @@ def tambah_scaling(koneksi, config_id, scale):
     if koneksi:
         try:
             cursor = koneksi.cursor()
-            cursor.execute("INSERT INTO Scaling (Config_ID, Pressure_In_Scale_Min, Pressure_In_Scale_Max, Pressure_A_Scale_Min, Pressure_A_Scale_Max, Pressure_B_Scale_Min, Pressure_B_Scale_Max, Flow_Scale_Min, Flow_Scale_Max, Temp_Scale_Min, Temp_Scale_Max, Curr_V_Scale_Min, Curr_V_Scale_Max, Aktual_Scale_Min, Aktual_Scale_Max, Curr_MA_Scale_Min, Curr_MA_Scale_Max) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)",
-                           (config_id, scale["Pressure_In_Scale_Min"], scale["Pressure_In_Scale_Max"], scale["Pressure_A_Scale_Min"], scale["Pressure_A_Scale_Max"], scale["Pressure_B_Scale_Min"], scale["Pressure_B_Scale_Max"], scale["Flow_Scale_Min"], scale["Flow_Scale_Max"], scale["Temp_Scale_Min"], scale["Temp_Scale_Max"], scale["Curr_V_Scale_Min"], scale["Curr_V_Scale_Max"], scale["Aktual_Scale_Min"], scale["Aktual_Scale_Max"], scale["Curr_MA_Scale_Min"], scale["Curr_MA_Scale_Max"]))
+            cursor.execute("INSERT INTO Scaling (Config_ID, Pressure_In_Scale_Min, Pressure_In_Scale_Max, Pressure_A_Scale_Min, Pressure_A_Scale_Max, Pressure_B_Scale_Min, Pressure_B_Scale_Max, Flow_Scale_Min, Flow_Scale_Max, Temp_Scale_Min, Temp_Scale_Max, Curr_V_Scale_Min, Curr_V_Scale_Max, Aktual_Scale_Min, Aktual_Scale_Max, Curr_MA_Scale_Min, Curr_MA_Scale_Max, Press_Com_Scale_Min, Press_Com_Scale_Max, Press_Aktual_Scale_Min, Press_Aktual_Scale_Max) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)",
+                           (config_id, scale["Pressure_In_Scale_Min"], scale["Pressure_In_Scale_Max"], scale["Pressure_A_Scale_Min"], scale["Pressure_A_Scale_Max"], scale["Pressure_B_Scale_Min"], scale["Pressure_B_Scale_Max"], scale["Flow_Scale_Min"], scale["Flow_Scale_Max"], scale["Temp_Scale_Min"], scale["Temp_Scale_Max"], scale["Curr_V_Scale_Min"], scale["Curr_V_Scale_Max"], scale["Aktual_Scale_Min"], scale["Aktual_Scale_Max"], scale["Curr_MA_Scale_Min"], scale["Curr_MA_Scale_Max"], scale["Press_Com_Scale_Min"], scale["Press_Com_Scale_Max"], scale["Press_Aktual_Scale_Min"], scale["Press_Aktual_Scale_Max"]))
             koneksi.commit()
         except sqlite3.Error as e:
             print(f"Kesalahan dalam menambahkan scaling: {e}")
--- main.py
+++ main.py
@@ -25,7 +25,6 @@ class MainApp(QObject):
 
         # Buat tabel konfigurasi jika belum ada
         buat_tabel_konfigurasi(self.koneksi)
-        buat_tabel_pengukuran(self.koneksi)
         buat_tabel_batasan(self.koneksi)
         buat_tabel_switch(self.koneksi)
         buat_tabel_scaling(self.koneksi)
@@ -35,12 +34,13 @@ class MainApp(QObject):
 
         # Ambil data konfigurasi dari database
         self.daftar_konfigurasi = self.ambil_daftar_konfigurasi()
-        self.daftar_ain = self.ambil_daftar_ain()
+        self.daftar_ain = [(0, 1, 2, 3, 4, 5, 6, 7, 8, 9)]
         self.daftar_min = self.ambil_daftar_min()
         self.daftar_max = self.ambil_daftar_max()
         self.daftar_min_scale = self.ambil_daftar_min_scaling()
         self.daftar_max_scale = self.ambil_daftar_max_scaling()
         self.daftar_switch = self.ambil_daftar_switch()
+        self.daftar_state = self.ambil_daftar_state()
         
         # Setup Parameter dan Value
         self.keys = ['press_in', 'press_a', 'press_b', 'flow', 'temp', 'curr_v', 'aktual', 'curr_ma', 'press_comm', 'press_actual']
@@ -67,17 +67,6 @@ class MainApp(QObject):
             config_id = tambah_konfigurasi(self.koneksi, "Default")
 
             # Tambahkan data pengukuran dan data batasan sesuai dengan ID konfigurasi
-            data_pengukuran = {
-                "Pressure_In": 0,
-                "Pressure_A": 1,
-                "Pressure_B": 2,
-                "Flow": 3,
-                "Temp": 4,
-                "Curr_V": 5,
-                "Aktual": 6,
-                "Curr_MA": 7
-            }
-
             data_batasan = {
                 "Pressure_In_Min": 0,
                 "Pressure_In_Max": 100,
@@ -89,12 +78,16 @@ class MainApp(QObject):
                 "Flow_Max": 100,
                 "Temp_Min": 0,
                 "Temp_Max": 100,
-                "Curr_V_Min": -5,
-                "Curr_V_Max": 5,
-                "Aktual_Min": -5,
-                "Aktual_Max": 5,
-                "Curr_MA_Min": -5,
+                "Curr_V_Min": -10,
+                "Curr_V_Max": 10,
+                "Aktual_Min": -10,
+                "Aktual_Max": 10,
+                "Curr_MA_Min": 0,
                 "Curr_MA_Max": 5,
+                "Press_Com_Min": 0,
+                "Press_Com_Max": 5,
+                "Curr_Aktual_Min": 0,
+                "Curr_Aktual_Max": 5,
             }
             
             data_scaling = {
@@ -114,6 +107,10 @@ class MainApp(QObject):
                 "Aktual_Scale_Max": 5,
                 "Curr_MA_Scale_Min": -5,
                 "Curr_MA_Scale_Max": 5,
+                "Press_Com_Scale_Min": 0,
+                "Press_Com_Scale_Max": 5,
+                "Curr_Aktual_Scale_Min": 0,
+                "Curr_Aktual_Scale_Max": 5,
             }
 
             data_switch = {
@@ -122,12 +119,55 @@ class MainApp(QObject):
                 "Btn3": 2,
                 "Btn4": 3,
                 "Btn5": 4,
-                "Btn6": 5
+                "Btn6": 5,
+                "Btn7": 6,
+                "Btn8": 7,
+                "Btn9": 8,
+                "Btn10": 9,
+                "Btn11": 10,
+                "Btn12": 11,
+                "Btn13": 12,
+                "Btn14": 13,
+                "Btn15": 14,
+                "Btn16": 15,
+                "Btn17": 16,
+                "Btn18": 17,
+                "Btn19": 18,
+                "Btn20": 19,
+                "Btn21": 20,
+                "Btn22": 21,
+                "Btn23": 21
             }
 
-            tambah_pengukuran(self.koneksi, config_id, data_pengukuran)
+            data_state = {
+                "Btn1_State": 0,
+                "Btn2_State": 0,
+                "Btn3_State": 0,
+                "Btn4_State": 0,
+                "Btn5_State": 0,
+                "Btn6_State": 0,
+                "Btn7_State": 0,
+                "Btn8_State": 0,
+                "Btn9_State": 0,
+                "Btn10_State": 0,
+                "Btn11_State": 0,
+                "Btn12_State": 0,
+                "Btn13_State": 0,
+                "Btn14_State": 0,
+                "Btn15_State": 0,
+                "Btn16_State": 0,
+                "Btn17_State": 0,
+                "Btn18_State": 0,
+                "Btn19_State": 0,
+                "Btn20_State": 0,
+                "Btn21_State": 0,
+                "Btn22_State": 0,
+                "Btn23_State": 0
+            }
+
             tambah_batasan(self.koneksi, config_id, data_batasan)
             tambah_switch(self.koneksi, config_id, data_switch)
+            tambah_state(self.koneksi, config_id, data_state)
             tambah_scaling(self.koneksi, config_id, data_scaling)
 
     # Fungsi untuk mengambil daftar konfigurasi dari database
@@ -135,12 +175,6 @@ class MainApp(QObject):
         cursor = self.koneksi.cursor()
         cursor.execute("SELECT Name FROM Configurations")
         return [konfigurasi[0] for konfigurasi in cursor.fetchall()]
-
-    # Fungsi untuk mengambil daftar pengukuran dari database
-    def ambil_daftar_ain(self):
-        cursor = self.koneksi.cursor()
-        cursor.execute("SELECT * FROM Measurements")
-        return [i[2:] for i in cursor.fetchall()]
 
     # Fungsi untuk mengambil min dan max dari database
     def ambil_daftar_min(self):
@@ -168,6 +202,12 @@ class MainApp(QObject):
     def ambil_daftar_switch(self):
         cursor = self.koneksi.cursor()
         cursor.execute("SELECT * FROM Switch")
+        return [konfigurasi[2:] for konfigurasi in cursor.fetchall()]
+
+    # Fungsi untuk mengambil daftar state dari database
+    def ambil_daftar_state(self):
+        cursor = self.koneksi.cursor()
+        cursor.execute("SELECT * FROM State")
         return [konfigurasi[2:] for konfigurasi in cursor.fetchall()]
 
     newValue = pyqtSignal('QVariantList')
