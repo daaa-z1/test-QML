ed73f29567eda7bacf3d1c1f4988240af7d1b60c
daaaz1 <daaa-z1@users.noreply.github.com>
2023-11-28 14:17:03 +0700

    asdw

--- main.py
+++ main.py
@@ -238,7 +238,7 @@ class MainApp(QObject):
         calculated_values = [(max_values[i] - min_values[i]) / (max_scale[i] - min_scale[i]) * (value[i] - min_scale[i]) for i in range(len(value))]
         calculated_values = [max(min(value, max_val), min_val) for value, max_val, min_val in zip(calculated_values, max_values, min_values)]
         calculated_values = [int(value) for value in calculated_values]
-        self.value = {key: calculated_values[i] for i, key in enumerate(self.keys)}
+        self.value = {key: calculated_values[i] for i, key in enumerate(keys)}
         
     # UPDATE _parameter dari TextField
     @pyqtSlot(str, str, float)
