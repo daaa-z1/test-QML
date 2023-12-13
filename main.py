ef27e83ff9dd8a38cc9b1fd223b7d620107ef70b
daaaz1 <daaa-z1@users.noreply.github.com>
2023-12-13 15:49:41 +0700

    zdvcds

--- main.py
+++ main.py
@@ -238,7 +238,7 @@ class MainApp(QObject):
         max_scale = [self._parameter[key]['maxScale'] for key in keys]
         min_values = [self._parameter[key]['minValue'] for key in keys]
         max_values = [self._parameter[key]['maxValue'] for key in keys]
-        calculated_values = [(max_values[i] - min_values[i]) / (max_scale[i] - min_scale[i]) * (value[i] - min_scale[i]) for i in range(len(value))]
+        calculated_values = [(max_values[i] - min_values[i]) / (max_scale[i] - min_scale[i]) * (value[i] - min_scale[i] + min_values[i]) for i in range(len(value))]
         calculated_values = [max(min(value, max_val), min_val) for value, max_val, min_val in zip(calculated_values, max_values, min_values)]
         calculated_values = [int(value) for value in calculated_values]
         self.value = {key: calculated_values[i] for i, key in enumerate(keys)}
